98fd1ae490b4a7a2558f70c7ecdf739a37cec80d
Likhith007-ai <likhithsai7979@gmail.com>
2023-12-26 11:11:28 +0530

    “ME_QTRIPDYNAMIC_MODULE_CITIES”  (Crio.Do)

--- frontend/conf/index.js
+++ frontend/conf/index.js
@@ -1,4 +1,9 @@
 
-const config = { backendEndpoint: "http://65.0.40.89:8082" };
+// const config = { backendEndpoint:"http://localhost:8082" };
 
-export default config;
+// export default config;
+  
+// File: frontend/conf/index.js
+const config = { backendEndpoint: "http://52.66.225.101:8082" };
+
+export default config;  
--- frontend/modules/landing_page.js
+++ frontend/modules/landing_page.js
@@ -1,7 +1,9 @@
-import config from "../conf/index.js";
+// import { elementRoles } from "aria-query";
+import config from "../conf/index";
 
 async function init() {
   //Fetches list of all cities along with their images and description
+  console.log(`API endpoint to fetch Cities data: ${config.backendEndpoint}/cities`);
   let cities = await fetchCities();
 
   //Updates the DOM with the cities
@@ -16,14 +18,50 @@ async function init() {
 async function fetchCities() {
   // TODO: MODULE_CITIES
   // 1. Fetch cities using the Backend API and return the data
-
+  try{
+  let response = await fetch(`${config.backendEndpoint}/cities`);
+  let user = await response.json();
+   return user;
+}catch(e) {
+  return null;
 }
+}
+fetchCities();
 
 //Implementation of DOM manipulation to add cities
 function addCityToDOM(id, city, description, image) {
   // TODO: MODULE_CITIES
   // 1. Populate the City details and insert those details into the DOM
+  let cityDetailsSection = document.getElementById("data");
+  cityDetailsSection.classList.add("d-flex","flex-wrap","justify-content-center");//----------------
+  
+  let add = document.createElement("div");
+  add.classList.add("col-12", "col-lg-3", "col-md-4","col-sm-6","tile","text-center","mx-0","my-4");
 
+  let link = document.createElement("a");
+  link.id = id;
+  link.href ="pages/adventures/?city=" + id;
+  
+
+  let division = document.createElement("div");
+  division.className = "tile-text";
+
+  let img = document.createElement("img");
+  img.classList.add("img-thumbnail");
+  img.src = image;
+
+  let title = document.createElement("h5");
+  title.innerText = city;
+
+  let destination = document.createElement("p");
+  destination.innerText = description;
+
+  link.appendChild(img);
+  division.appendChild(title);
+  division.appendChild(destination);
+  link.appendChild(division);
+  add.appendChild(link);
+  cityDetailsSection.appendChild(add);
 }
 
 export { init, fetchCities, addCityToDOM };
